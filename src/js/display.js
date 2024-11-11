0e6bb0b1814775c7312fdc5498d4e90d91a82f0f
dchen046 <dchen046@gmail.com>
2024-11-11 17:27:31 -0500

    finished basic weather card

---
+++ src/js/display.js
@@ -0,0 +1,53 @@
+import { weatherData } from "./search";
+
+const recents = [];
+
+export const addWeatherCard = (data) => {
+    const results = document.getElementById('search-results');
+    results.appendChild(createWeatherCard(data));
+}
+
+const createWeatherCard = (data) => {
+    const card = document.createElement('div');
+    const cardClasses = 'd-flex flex-column justify-content-center text-center container'.split(' ');
+    card.classList.add(...cardClasses);
+    const info = [createDate(data), createIcon(data), createTemp(data)];
+    info.forEach( (x) => {
+        card.appendChild(x);
+    });
+    return card;
+}
+
+const createDate = (data) => {
+    const p = document.createElement('h4');
+    const date = data.datetime.split('-');
+    p.textContent = `${date[1]}/${date[2]}`;
+    return p;
+}
+
+const createIcon = (data) => {
+    const condition = data.conditions;
+    const icon = document.createElement('i');
+    let iconClass;
+    let color;
+    if (condition.includes('Clear')) {
+        iconClass = 'fas fa-sun'.split(' ');
+        color = '#FFD43B'
+    } else if (condition.includes('Rain')) {
+        iconClass = 'fas fa-umbrella'.split(' ');
+        color = '#74C0FC'
+    } else {
+        iconClass = 'fas fa-cloud'.split(' ');
+    }
+
+    iconClass.push('m-3', 'fa-xl');
+    icon.classList.add(...iconClass);
+    icon.setAttribute('style', `color: ${color}`);
+    return icon;
+}
+
+const createTemp = (data) => {
+    const temp = document.createElement('h4');
+    temp.textContent = data.temp;
+    return temp; 
+}
--- src/js/search.js
+++ src/js/search.js
@@ -1,8 +1,53 @@
 import { apiKey } from "../stuffs/config";
+import { addWeatherCard } from "./display";
 
-export const getWeatherData = async (location) => {
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+export let weatherData;
+
+const getWeatherData = async (location) => {
+    try {
+        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        document.getElementById('search-error').textContent = '';
+        return data;
+    } catch (err) {
+        const msg = "Could not find city"
+        document.getElementById('search-error').textContent = msg;
+        throw msg;
+    }
+    
+}
+
+const displayCards = (amount) => {
+    for (let i = 0; i < amount; ++i) {
+        addWeatherCard(weatherData.days[i]);
+    }
+}
+
+const setSearchBtn = () => {
+    const btn = document.getElementById("search-btn");
+    btn.addEventListener('click', async () => {
+        try {
+            document.getElementById('search-results').textContent = '';
+            const city = document.getElementById('location').value;
+            weatherData = await getWeatherData(city);
+            displayCards(5);
+        } catch (err) {
+            console.log('aaa');
+        }
+    });
+};
+
+const setSearchBarEvent = () => {
+    const bar = document.getElementById('location');
+    bar.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            document.getElementById('search-btn').click();
+        }
+    })
+}
+
+export const initSearch = () => {
+    setSearchBtn();
+    setSearchBarEvent();
 }

d033f89a3a9f66c55278a9ebd86b8783ddbea974
dchen046 <dchen046@gmail.com>
2024-11-11 13:52:06 -0500

    finished basic layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css'
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'

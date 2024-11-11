6c858c43998897bbd248f99a5a0a1547772675b3
dchen046 <dchen046@gmail.com>
2024-11-11 15:44:42 -0500

    moved variables

--- src/js/search.js
+++ src/js/search.js
@@ -1,7 +1,8 @@
+import { apiKey } from "../stuffs/config";
+
 export const getWeatherData = async (location) => {
-    const apiKey = 'VE2WD7JC5TQSNF9KRY5T75SYT'
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    return data;
 }
